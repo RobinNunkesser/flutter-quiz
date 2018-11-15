5fb025e4627744cfdd6f8cc3d88f50c71eeb385a
Robin Nunkesser <robin.nunkesser@hshl.de>
2018-11-15 09:15:43 +0100

    Animation completed

--- quiz/lib/my_home_page.dart
+++ quiz/lib/my_home_page.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
-import 'package:quiz/platform_scaffold.dart';
 import 'package:quiz/platform_button.dart';
+import 'package:quiz/platform_scaffold.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -11,39 +12,47 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  var _questions = [['Das Videospiel Donkey Kong sollte ursprünglich Popeye als Hauptfigur haben.',true],
-                    ['Die Farbe Orange wurde nach der Frucht benannt.', true],
-                    ['In der griechischen Mythologie ist Hera die Göttin der Ernte.', false],
-                    ['Liechtenstein hat keinen eigenen Flughafen.', true],
-                    ['Die meisten Subarus werden in China hergestellt.', false]];
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<double> animation;
+
+  var _questions = [
+    [
+      'Das Videospiel Donkey Kong sollte ursprünglich Popeye als Hauptfigur haben.',
+      true
+    ],
+    ['Die Farbe Orange wurde nach der Frucht benannt.', true],
+    ['In der griechischen Mythologie ist Hera die Göttin der Ernte.', false],
+    ['Liechtenstein hat keinen eigenen Flughafen.', true],
+    ['Die meisten Subarus werden in China hergestellt.', false]
+  ];
   int _index = 0;
+  bool _areButtonsDisabled = false;
+
   String get _question => _questions[_index][0];
-  var _response = 'TEXT';
+  var _response = '';
 
-  void _skip() {
-    setState(() {
-      _index = ++_index % _questions.length;
-    });
-  }
-
-  void _evaluateAnswerFalse() => _evaluateAnswer(false);
-  void _evaluateAnswerTrue() => _evaluateAnswer(true);
-
-  void _evaluateAnswer(bool answer) {
-    setState(() {
-      if (answer==_questions[_index][1]) {
-        _response = "Richtig!";
-      } else {
-        _response = "Falsch!";
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1000),
+    );
+    animation = Tween(begin: 0.0, end: 1.0).animate(_controller)
+    ..addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _controller.reset();
+        setState(() {
+          _areButtonsDisabled = false;
+        });
       }
     });
-    _skip();
   }
 
   @override
   Widget build(BuildContext context) {
-
     var questionSection = Expanded(
       child: Container(
         padding: const EdgeInsets.all(16.0),
@@ -51,16 +60,21 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
 
+    var responseSection = FadeTransition(
+               opacity: animation,
+               child: Text('$_response'),
+    );
+
     var buttonSection = Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
         PlatformButton(
           child: Text('Falsch'),
-          onPressed: _evaluateAnswerFalse,
+          onPressed: _areButtonsDisabled ? null : _evaluateAnswerFalse,
         ),
         PlatformButton(
           child: Text('Richtig'),
-          onPressed: _evaluateAnswerTrue,
+          onPressed: _areButtonsDisabled ? null : _evaluateAnswerTrue,
         ),
       ],
     );
@@ -71,15 +85,44 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           children: [
             questionSection,
-            Text('$_response'),
+            responseSection,
             buttonSection,
             PlatformButton(
               child: Text('Überspringen'),
-              onPressed: _skip,
+              onPressed: _areButtonsDisabled ? null : _skip,
             ),
           ],
         ),
       ),
     );
+  }
+
+  @override
+     dispose() {
+       _controller.dispose();
+       super.dispose();
+     }
+
+  void _skip() {
+    setState(() {
+      _index = ++_index % _questions.length;
+    });
+  }
+
+  void _evaluateAnswerFalse() => _evaluateAnswer(false);
+
+  void _evaluateAnswerTrue() => _evaluateAnswer(true);
+
+  void _evaluateAnswer(bool answer) {
+    setState(() {
+      _areButtonsDisabled = true;
+      if (answer == _questions[_index][1]) {
+        _response = "Richtig!";
+      } else {
+        _response = "Falsch!";
+      }
+    });
+    _controller.forward();
+    _skip();
   }
 }
