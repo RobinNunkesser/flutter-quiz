ffc1eb84dc8d518951fb620226c175ba31d8da0c
Robin Nunkesser <robin.nunkesser@hshl.de>
2018-12-04 23:01:06 +0100

    Refactoring

--- quiz/lib/my_app.dart
+++ quiz/lib/my_app.dart
@@ -5,9 +5,9 @@ import 'package:quiz/my_home_page.dart';
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new PlatformApp(
+    return PlatformApp(
       title: 'Quiz',
-      home: new MyHomePage(title: 'Quiz'),
+      home: MyHomePage(title: 'Quiz'),
     );
   }
 }
